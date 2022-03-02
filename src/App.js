6174d7340b165a52f1e076667a87b88a083f3629
Justin Blalock <justin.m.blalock@gmail.com>
2022-03-02 12:01:05 -0600

    class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
---
+++ src/Details.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import { useParams } from "react-router-dom";
+
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h1>
+            {animal} - {breed} - {city}, {state}
+          </h1>
+          <button>Adopt</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
